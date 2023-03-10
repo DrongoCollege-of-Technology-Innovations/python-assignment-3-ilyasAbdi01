617ae0fd60950a7b013eb127ef62c2adaf337c8c
ilyasAbdi01 <ilyasyr@gmail.com>
2023-03-10 10:33:59 +0300

    submited

--- exercise.py
+++ exercise.py
@@ -1,3 +1,10 @@
 # create a function called get_average() and calculate the average of the numbers below and return the average
 # values = [61, 20, 45, 63, 96, 71, 6, 8, 72, 22, 97, 7, 46, 11, 15, 74, 81, 69, 70, 26]
 
+def get_averege():
+    values = [61, 20, 45, 63, 96, 71, 6, 8, 72, 22, 97, 7, 46, 11, 15, 74, 81, 69, 70, 26]
+    averege = sum(values)/ len(values)
+    print('The averege is', averege)
+    return averege
+
+get_averege()
